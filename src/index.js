2496a84d3b9984ad3465e1546519f6ee0083d01f
mellmontenegro <56005764+mellmontenegro@users.noreply.github.com>
2019-12-02 15:49:34 +0000

    Created initial function to capture real data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 function showTemperature(response) {
-  console.log(response.data.main.temp);
-
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${Math.round(response.data.main.temp)}`;
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let tempmaxElement = document.querySelector("#tempmax");
+  let tempminElement = document.querySelector("#tempmin");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  tempmaxElement.innerHTML = Math.round(response.data.main.temp_max);
+  tempminElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 
 let city = "Lisbon";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=875ea8327db11c913ffcc92c93a23805&units=metric`;
+let apiKey = "875ea8327db11c913ffcc92c93a23805";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(url).then(showTemperature);
+axios.get(apiUrl).then(showTemperature);
